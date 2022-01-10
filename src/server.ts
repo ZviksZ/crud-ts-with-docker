cd494778a673961e325a1040becd4d2483063ddd
Nikita Kapshtyk <viks2332@gmail.com>
2022-01-10 15:57:59 +0300

    adding router

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 import db from './config/database.config'
-import TodoValidator from './validator'
-import Middleware from "./middleware";
-import TodoController from "./controller";
+import todoRouter from './route';
 
 db.sync().then(() => {
    console.log('connect to db')
@@ -13,31 +11,7 @@ const port = 9000
 
 app.use(express.json())
 
-app.post('/create',
-   TodoValidator.checkCreateTodo(),
-   Middleware.handleValidationError,
-   TodoController.create
-)
-
-app.get('/read',
-   TodoValidator.checkReadTodo(),
-   Middleware.handleValidationError,
-   TodoController.readPagination)
-
-app.get('/read/:id',
-   TodoValidator.checkIdParam(),
-   Middleware.handleValidationError,
-   TodoController.readById)
-
-app.put('/update/:id',
-   TodoValidator.checkIdParam(),
-   Middleware.handleValidationError,
-   TodoController.update)
-
-app.delete('/delete/:id',
-   TodoValidator.checkIdParam(),
-   Middleware.handleValidationError,
-   TodoController.delete)
+app.use(todoRouter)
 
 app.listen(port, () => {
    console.log('Server is running on port ' + port)
